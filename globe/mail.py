9dcc635d0d5239928415ecab7a5ddb5387f98dea
Josh Tait <Josh.tait3@gmail.com>
2017-10-09 18:08:54 +0100

    add mail.py

---
+++ globe/mail.py
@@ -0,0 +1,8 @@
+from flask_mail import Message
+from globe import app, mail
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender[0], recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
