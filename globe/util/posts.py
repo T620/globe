48b0ffcf123238e406884fbc546dc26f778b2147
Josh Tait <Josh.tait3@gmail.com>
2017-11-24 15:35:27 +0000

    add 'appreciate' function

--- globe/util/posts.py
+++ globe/util/posts.py
@@ -5,3 +5,18 @@ def filter_by(primaryCriteria, secondaryCriteria):
 
 #marks a post as appreaciated (featured for that location)
 def appreciate(post):
+	#a post is appreciated if it reaches over 200 likes for a particular location
+
+	from globe.models import Post
+
+	post = Post.query.filter_by(id=postID).first()
+
+	if post.likes > 200:
+		if post.appreciated == "True":
+			print 'post is already appreciated, skipping'
+		else:
+			post.appreciated = "True"
+			db.session.add(post)
+			db.session.commit()
+	else:
+		return False
