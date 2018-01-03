e4a02dd276a0dbd8b483967a8f011543d41a00ac
Josh Tait <Josh.tait3@gmail.com>
2018-01-03 16:26:26 +0000

    add core files again

---
+++ globe/models.py
@@ -0,0 +1,151 @@
+import os
+from globe import app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app.config.from_envvar('APP_CONFIG_FILE')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE_URL']
+
+
+
+class User(db.Model):
+	__table_args__ = {'extend_existing': True}
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	email = db.Column(db.String(60), unique=True)
+	username = db.Column(db.String(60), unique=True)
+	forename = db.Column(db.String(20))
+	surname = db.Column(db.String(20))
+	city = db.Column(db.String(40))
+	followers = db.Column(db.String(6000))
+	following = db.Column(db.String(6000))
+	biography = db.Column(db.String(200))
+
+
+	def __init__(self, id, email, username, forename, surname, city, followers, following, biography):
+		self.id = id
+		self.email=email
+		self.username=username
+		self.forename=forename
+		self.surname=surname
+		self.city=city
+		self.followers=followers
+		self.following=following
+		self.biography=biography
+
+
+	def __repr__(self):
+		return '<Username %r>' % self.username
+
+
+class UserAuth(db.Model):
+	__table_args__ = {'extend_existing': True}
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	email = db.Column(db.String(60), unique=True)
+	username = db.Column(db.String(60), unique=True)
+	password=db.Column(db.String(60))
+	registeredOn = db.Column(db.String(60))
+	confirmationToken = db.Column(db.String(60))
+	passwordToken = db.Column(db.String(60))
+	lastLogin = db.Column(db.String(60))
+	lastIPUsed = db.Column(db.String(15))
+	verified = db.Column(db.Boolean)
+
+
+	def is_authenticated(self):
+		return True
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return unicode(self.id)
+
+
+	def __init__(self, id, email, username, password, registeredOn, confirmationToken, passwordToken, lastLogin, lastIPUsed, verified):
+		self.id = id
+		self.email = email
+		self.username = username
+		self.password = password
+		self.registeredOn = registeredOn
+		self.confirmationToken = confirmationToken
+		self.passwordToken = passwordToken
+		self.lastLogin = lastLogin
+		self.lastIPUsed = lastIPUsed
+		self.verified = verified
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+
+
+class Post(db.Model):
+	__table_args__ = {'extend_existing': True}
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	username = db.Column(db.String(60))
+	postedOn = db.Column(db.String(60))
+	postContent = db.Column(db.String(60))
+	likes = db.Column(db.String(5))
+	image = db.Column(db.String(60))
+	city = db.Column(db.String(15))
+	appreaciated = db.Column(db.Boolean)
+
+	def __init__(self, id, email, username, postedOn, postContent, likes, image, city, appreaciated):
+		self.id = id
+		self.username=username
+		self.postedOn = postedOn
+		self.postContent = postContent
+		self.likes = likes
+		self.image = image
+		self.city = city
+		self.appreaciated = appreaciated
+
+
+	def __repr__(self):
+		return '<Id %r>' % self.id
+
+
+class Admin(db.Model):
+	__table_args__ = {'extend_existing': True}
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	email = db.Column(db.String(60), unique=True)
+	username = db.Column(db.String(60))
+	registeredOn = db.Column(db.String(60))
+	confirmationToken = db.Column(db.String(60))
+	passwordToken = db.Column(db.String(60))
+	lastLogin = db.Column(db.String(60))
+	lastIPUsed = db.Column(db.String(15))
+
+
+	def __init__(self, id, email, username, registeredOn, confirmationToken, passwordToken, lastLogin, lastIPUsed):
+		self.id = id
+		self.email = email
+		self.username = username
+		self.registeredOn = registeredOn
+		self.confirmationToken = confirmationToken
+		self.passwordToken = passwordToken
+		self.lastLogin = lastLogin
+		self.lastIPUsed=lastIPUsed
+
+
+	def is_authenticated(self):
+		return True
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return unicode(self.id)
+
+	def __repr__(self):
+		return '<Username %r>' % self.username
---
+++ globe/views.py
@@ -0,0 +1,199 @@
+import os, uuid, random, string
+from globe import app, db, mail
+from flask import render_template, request, redirect, url_for, session, abort
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS, cross_origin
+
+CORS(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view =  "login"
+
+
+bcrypt = Bcrypt(app)
+
+
+@login_manager.user_loader
+def load_user(userid):
+	from models import UserAuth
+	user = UserAuth.query.filter(UserAuth.id==userid).first()
+	session['user'] = user.id
+	print "[INFO]: Current User: %s" % session['user']
+	print "aka %s" % user.username
+	return user
+
+@app.route('/')
+def load_index():
+	return render_template("index.html")
+
+@app.route("/feed/")
+@cross_origin()
+def load_feed():
+	return render_template("feed.html")
+
+
+
+@app.route("/explore/")
+def explore():
+	return 'enter a country'
+
+
+@app.route("/map/")
+def load_map():
+	from models import Post
+	products = Post.query.filter_by(status="Available").all()
+	count = Post.query.filter_by(status="Available").count()
+
+	#Grab the API key for GMaps
+	key = os.environ.get('MAPS_API_KEY')
+
+	return render_template("map.html", products=products, key=key, count=count)
+
+
+@app.route("/user/")
+def redr_to_profile():
+	return redirect(url_for('load_int_user'))
+
+
+#this is the current user's personal profile
+@login_required
+@app.route("/user/profile/")
+def load_int_user():
+	#grab the users details
+	from models import User
+	user = User.query.filter_by(id=session['user']).first_or_404()
+
+	if user is not None:
+		return render_template("user/profile.html", user=user)
+	else:
+		return "User not found after logging in??"
+
+
+#external user (not the person logged in, someone else)
+@app.route("/user/<username>")
+def load_ext_user(username):
+	#grab the user's basic profile info
+	from models import User, Post, Booking
+	user = User.query.filter_by(username=unicode.title(username)).first_or_404()
+
+	return render_template("user/ext_profile.html", user=user, itemsBorrowed=None)
+
+
+
+@app.route("/user/posts/")
+def redr_to_all():
+	#default is /all/, so redirect the user to prevent 404
+	return redirect(url_for('load_users_posts', _filter='all'))
+
+
+@app.route('/login/', methods=["GET", "POST"])
+def login():
+	if request.method=="GET":
+		return render_template("/user/login.html")
+	else:
+		username = request.form['username']
+		password = request.form['password']
+
+		from util import user
+		userID = user.get_id(username)
+
+		if user.exists(userID):
+			if password_hash_matches(userID, password):
+				login_user(user)
+			else:
+				return "password invalid :("
+		else:
+			return "User does not exist."
+
+
+@app.route('/register/', methods=["GET", "POST"])
+def register():
+	if request.method=="POST":
+		from util import user
+
+		newUser = {
+			"forename": request.form['forename'],
+			"surname": request.form['surname'],
+			"email": request.form['email'],
+			"password": request.form['password'],
+			"city": request.form['city']
+		}
+
+		if user.register(newUser):
+			return render_template('user/register_step-2.html', username=newAccountAuth.username)
+		else:
+			return "error."
+	else:
+		return render_template("register.html")
+
+
+
+@app.route("/register/auth/")
+def confirm_new_user():
+	#user didnt post anything, so they've accessed this URL from their email
+	token = request.args.get("token", None)
+	username = request.args.get("username", None)
+
+	if token is not None and username is not None:
+		#no need to check CSRF here
+		from models import UserAuth
+		from util import user, email
+
+		if util.authorise(token, username):
+			#send the user a confirmation email
+			from mail import send_email
+			subject = "Globe: New Account"
+			sender=["no-reply@globe.com"]
+			recipients=user.email.split(" ")
+			text_body="Welcome!"
+			html_body=render_template("user/new_account.html", username=username)
+
+			return render_template('user/register_step-3.html')
+		else:
+			return "error"
+
+	else:
+		return "invalid token/username combination"
+
+
+
+
+@app.route("/register/resend-email", methods=["GET", "POST"])
+def resend_email():
+	if request.method == "POST":
+		username = request.args.get("username", None)
+		if username is not None:
+			#create a new confirm token and send them it.
+			from models import UserAuth
+			user = UserAuth.query.filter_by(username=username).first_or_404()
+
+			confirmToken = uuid.uuid4().hex
+			user.confirmationToken = confirmToken
+
+			db.session.add(user)
+			db.session.commit()
+
+			#resend the email
+			emailTo = user.email.split(" ")
+
+			#send the user a confirmation email
+			from mail import send_email
+			subject = "Globe: New Account"
+			sender=["no-reply@globe.com"]
+			recipients=emailTo
+			text_body="Welcome!"
+			html_body=render_template("user/new_account.html", username=username)
+
+			return "email re-sent!"
+
+	else:
+		abort(403)
+
+
+
+
+if __name__ == '__app__':
+	app.debug=True
+	app.run(host='0.0.0.0', port=5000)
