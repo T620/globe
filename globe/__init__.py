e540f58b3a639aeeb2b230ac8f011d77dfdaa9c1
Josh Tait <Josh.tait3@gmail.com>
2018-01-04 21:30:39 +0000

    fix merge text

--- globe/__init__.py
+++ globe/__init__.py
@@ -5,27 +5,10 @@ from flask_bcrypt import Bcrypt
 from flask_mail import Mail, Message
 
 
-<<<<<<< HEAD
 app = Flask(__name__)
 
-#load the default settings first
-app.config.from_pyfile("../config/config.py")
-#load the secrets
-app.config.from_envvar('APP_CONFIG_FILE')
-
-db = SQLAlchemy(app)
-
-mail=Mail(app)
-
-import globe.models
-
-print db
-
-=======
-app = Flask(__globe__)
-
 #load the default settings
-app.config.from_pyfile("../config/secrets.py")
+app.config.from_pyfile("../config/config.py")
 
 
 
@@ -35,5 +18,4 @@ mail = Mail(app)
 print db
 
 import globe.models
->>>>>>> master
 import globe.views
