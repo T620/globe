8e513c4a01d7e635de8f3fd6985f41f848864bb0
Josh Tait <Josh.tait3@gmail.com>
2018-04-07 14:16:34 +0100

    tidied up code, fixed profile images

--- globe/util/_user.py
+++ globe/util/_user.py
@@ -20,30 +20,6 @@ def password_hash_matches(userid, password):
 		return False
 
 
-def set_default_photos(url):
-	import os, tinys3, string
-	from globe import app
-
-	#init conection to S3
-	conn = tinys3.Connection(os.environ['S3_PUB_KEY'], os.environ['S3_PRIVATE_KEY'], tls=True)
-
-	# create the user's s3 subdir + image path
-	#josh.tait912/profile/placeholder.jpg
-
-	photo = app.config['UPLOAD_FOLDER'] + "profiles/placeholder.jpg"
-	
-	if os.environ['FILE_STORAGE_LOC'] == "LOCAL":
-		print 'saving file locally'
-	else:
-		print 'saving file to S3'
-		#now the file and path are ready, save the file to the user's folder in s3
-		f = open(photo, 'rb')
-		conn.upload(url, f, os.environ['S3_BUCKET_NAME'])
-		#https://s3.aws.com/endpoint/bucket/static/user_uploads/josh.tait516/profile/placeholder.jpg
-		#photoUrl = os.environ['S3_ENDPOINT'] + "/" os.environ['S3_BUCKET_NAME'] + url
-
-		print "uploaded default profile photo, creating user_uploads subdir..."
-
 def register(newUser):
 	from globe import app, db
 	import os, tinys3, string
@@ -55,13 +31,11 @@ def register(newUser):
 	passwordToken = uuid.uuid4().hex
 	confirmToken = uuid.uuid4().hex
 
-	url = app.config['UPLOAD_FOLDER'] + str(userID) + "/profile/" + 'placeholder.jpg'
-	#url = '/static/user_uploads/' + str(userID) + "/profile/" + 'placeholder.jpg'
+	url = "/static/user_uploads/profiles/placeholder/placeholder.jpg"
+	#url = '/static/user_uploads/50123/profile/placeholder.jpg'
 
-	#photoUrl = "https://s3.us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-908893185885" + url
-
-	print url
-	set_default_photos(url)
+	stockImage = "http://" + os.environ['S3_ENDPOINT'] + "/" + os.environ['S3_BUCKET_NAME'] + url
+	print stockImage
 
 	newAccount = User(
 		id=userID,
@@ -75,36 +49,11 @@ def register(newUser):
 		city=newUser['city'],
 		biography="None",
 		verified="False",
-		photo=url
+		photo=stockImage
 	)
 
 	db.session.add(newAccount)
 	db.session.commit()
-
-	print 'user added, creating post subdirectory'
-
-	import os
-	subfolder = userID + "/profile/posts/"
-	folder = os.path.join(app.config['UPLOAD_FOLDER'], subfolder)
-	os.makedirs(folder)
-
-	print 'subdirectory added'
-
-
-
-
-def create_post_folder(userID, postID):
-	import os
-	subfolder = userID + "/profile/posts/" + postID
-
-	folder = os.path.join(app.config['UPLOAD_FOLDER'], subfolder)
-
-	try:
-		os.mkdir(folder)
-		return True
-	except:
-		return False
-
 
 def authorise(token, username):
 
